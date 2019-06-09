1676d65f48e9483022a265bf755531a6f5dba6f4
Websitechic Engineering <engineering@websitechic.biz>
2019-06-09 01:54:32 +0100

    Now funcional

--- src/JWTAuthServiceProvider.php
+++ src/JWTAuthServiceProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class JWTAuthServiceProvider extends ServiceProvider {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
         $this->publishes([
             // Config
             __DIR__.'/../config/BdJwt.php' => config_path('BdJwt.php'),
